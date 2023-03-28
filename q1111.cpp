0acb0390bd721d00e1e0c1c51a49bc9a979c8de1
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-03-28 23:59:22 +0530

    hue

--- q1111.cpp
+++ q1111.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
 using namespace std; 
 int main(){
-    int k = 5, index = 0;
-    int arr[5] = {1, 2, 3, 4, 5};
-    for (int i = 0; i < 5; i++){
-        index++;
-        if(arr[i]== k){
-            
-            
-            cout << "Element found : " <<index<< endl;
-            break;
-        }else{
-            cout << "element not found" << endl;
+    int arr[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    cout << "Unsorted array :\n";
+    for (int i = 0; i < 10; i++){
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+    for (int k = 1; k < 10; k++){
+        int temp = arr[k];
+        int j = k - 1;
+        if (j >= 0 && temp  <= arr[j]){
+            arr[j + 1] = arr[j];
+            j = j - 1; 
         }
+        arr[j + 1] = temp; 
+    }
+    cout<<"Sorted array : \n";
+    for (int i = 0; i < 10; i++){
+        cout << arr[i] << "\t"; 
     }
     return 0;
-
 }
