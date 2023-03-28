70c9a062a8b04b0a63a990b30a4ff2e0f4514fd5
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-03-28 20:41:49 +0530

    lil

---
+++ insrt.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[6]={18, 17,16,15,14,13}; 
+   cout<<"Unsorted array: \n";
+   for (int i = 0; i< 6; i++){
+       cout << arr[i] << "\t"; 
+   }
+   
+}
