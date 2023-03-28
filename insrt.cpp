ff78f0fe585fcc2d4ebec86a4f250853bd60a417
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-03-28 22:13:28 +0530

    lwl

--- insrt.cpp
+++ insrt.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int arr[6]={18, 17,16,15,14,13}; 
-   cout<<"Unsorted array: \n";
-   for (int i = 0; i< 6; i++){
-       cout << arr[i] << "\t"; 
-   }
-   cout << endl;
-   for (int k = 1; k < 6; k++)
-   {
-       int temp = arr[k];
-       int j = k-1;
-       while (j >= 0 && temp <= arr[j]){
-        arr[j+1] = arr[j];
-        j = j - 1; 
-       }
-       arr[j + 1] = temp;
-   }
-   cout << "Sorted array :\n ";
-   for (int i = 0; i < 6; i++){
-       cout << arr[i] << "\t"; 
-   }
+    int arr[6] = {6, 5, 4, 3, 2, 1}; 
+    cout<<"Unsorted array: \n";
+    for (int i = 0; i< 6; i++){
+        cout << arr[i] << "\t"; 
+
+    }
+    for (int k = 1; k < 6; k++){
+        int temp = arr[k];
+        int j = k - 1;
+        while(j >= 0 && temp <= arr[j]){
+            arr[j + 1] = arr[j];
+            j = j - 1; 
+
+        }
+        arr[j +1] = temp; 
+    }
+     cout<<"Sorted array : \n";
+     for (int i = 0; i < 6; i++){
+        cout << arr[i] << "\t"; 
+     }
 }
