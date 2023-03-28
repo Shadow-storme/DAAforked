b618e43262bc1b5f53d10984cf6e119426a8863f
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-03-28 20:48:37 +0530

    lql

--- insrt.cpp
+++ insrt.cpp
@@ -6,5 +6,19 @@ int main(){
    for (int i = 0; i< 6; i++){
        cout << arr[i] << "\t"; 
    }
-   
+   cout << endl;
+   for (int k = 1; k < 6; k++)
+   {
+       int temp = arr[k];
+       int j = k-1;
+       while (j >= 0 && temp <= arr[j]){
+        arr[j+1] = arr[j];
+        j = j - 1; 
+       }
+       arr[j + 1] = temp;
+   }
+   cout << "Sorted array :\n ";
+   for (int i = 0; i < 6; i++){
+       cout << arr[i] << "\t"; 
+   }
 }
