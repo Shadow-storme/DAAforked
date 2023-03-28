42bf816dad41989851ec0d5b396d542e91d37811
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-03-28 23:42:00 +0530

    lly

--- q1111.cpp
+++ q1111.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std; 
 int main(){
-    int k = 2, index = 0;
+    int k = 5, index = 0;
     int arr[5] = {1, 2, 3, 4, 5};
     for (int i = 0; i < 5; i++){
+        index++;
         if(arr[i]== k){
             
-            index++;
+            
             cout << "Element found : " <<index<< endl;
             break;
         }else{
